d37f0af4b43356965e14d6c1f6c0c074d90fbbd7
Dmayank297 <139100813+Dmayank297@users.noreply.github.com>
2024-11-19 22:34:31 +0530

    First Commit

--- app/src/main/java/com/example/mausam/ui/screens/HelpScreen.kt
+++ app/src/main/java/com/example/mausam/ui/screens/HelpScreen.kt
@@ -1,27 +1,63 @@
 package com.example.mausam.ui.screens
 
-import androidx.compose.foundation.Image
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.mausam.R
+import androidx.navigation.compose.rememberNavController
+
 
 @Composable
 fun HelpScreen(
-    navController: NavController
+    navController: NavController = rememberNavController()
 ) {
+    Scaffold(topBar = {
+        TopAppBar(
+            modifier = Modifier
+                .fillMaxWidth(),
+            title = {
 
+                Text(
+                    modifier = Modifier
+                        .padding(start = 24.dp),
+                    text = "Help Screen",
+                    fontSize = 16.sp,
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+
+                )
+            }
+
+        )
+    }
+
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Help Screen")
+        }
+    }
 }
 
-@Preview(showBackground = false)
+@Preview
 @Composable
-fun previewImage() {
-    val context = LocalContext.current
-    val navController: NavController = NavController(context = context)
-    HelpScreen(navController = navController)
+fun previewHelpScreen() {
+    HelpScreen()
 }
--- app/src/main/java/com/example/mausam/ui/screens/Settings.kt
+++ app/src/main/java/com/example/mausam/ui/screens/Settings.kt
@@ -1,12 +1,63 @@
 package com.example.mausam.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun Settings(
-    navController: NavController
+    navController: NavController = rememberNavController()
 ) {
-    Text(text = "Settings")
+    Scaffold(topBar = {
+        TopAppBar(
+            modifier = Modifier
+                .fillMaxWidth(),
+            title = {
+
+                androidx.compose.material.Text(
+                    modifier = Modifier
+                        .padding(start = 24.dp),
+                    text = "Settings Screen",
+                    fontSize = 16.sp,
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+
+                    )
+            }
+
+        )
+    }
+
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            androidx.compose.material.Text(text = "Settings Screen")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun previewSettingsScreen() {
+    Settings()
+
 }
